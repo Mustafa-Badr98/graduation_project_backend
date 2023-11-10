2e4639f2dab5e5ed7ee1c2700ade9bc82dadd0a9
AhmeedKadry <ahmed.Kadry.ramadan@gmail.com>
2023-11-11 01:41:14 +0200

    offers authentications

--- realtorSite2/offers/api/views.py
+++ realtorSite2/offers/api/views.py
@@ -1,11 +1,19 @@
 from rest_framework import generics
+from rest_framework import permissions
 from offers.models import Offer
 from .serializers import OfferSerializer
 
+
 class OfferList(generics.ListCreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
 
 class OfferDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
