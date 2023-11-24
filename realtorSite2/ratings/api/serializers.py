5e932953e4946996d8938730d7da040ab9330f48
Mustafa Badr <mustafabadr879@gmail.com>
2023-11-24 22:09:05 +0200

    serilized users in rate model..

--- realtorSite2/ratings/api/serializers.py
+++ realtorSite2/ratings/api/serializers.py
@@ -1,11 +1,12 @@
 
 from rest_framework import serializers
 from ratings.models import Rating
-
+from users.api.serializers4 import UserOfferSerializer
 
 
 class RatingSerializer(serializers.ModelSerializer):
-    # user=
+    user = UserOfferSerializer(read_only=True)
+    rated_by=UserOfferSerializer(read_only=True)
     class Meta:
         model = Rating
         fields = '__all__'
